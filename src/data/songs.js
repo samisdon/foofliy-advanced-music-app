b52147fe0d32760d2fff0e3553f23c15ef0a3fe1
aditya <singhaditya2206@gmail.com>
2026-01-22 14:11:49 +0530

    Create songs.js

---
+++ src/data/songs.js
@@ -0,0 +1,39 @@
+const songs = [
+  // Punjabi Hits
+  { id: 1, title: "Wavy", artist: "Karan Aujla" },
+  { id: 2, title: "Softly", artist: "Karan Aujla" },
+  { id: 3, title: "Winning Speech", artist: "Karan Aujla" },
+  { id: 4, title: "Back To Me", artist: "Arjan Dhillon" },
+  { id: 5, title: "Desi Hood", artist: "Prem Dhillon" },
+  { id: 6, title: "8 Asle", artist: "Navaan Sandhu" },
+  { id: 7, title: "Brown Munde", artist: "AP Dhillon" },
+  { id: 8, title: "Excuses", artist: "AP Dhillon" },
+  { id: 9, title: "With You", artist: "AP Dhillon" },
+  { id: 10, title: "Obsessed", artist: "Riar Saab" },
+  { id: 11, title: "One Love", artist: "Shubh" },
+  { id: 12, title: "Supreme", artist: "Shubh" },
+  { id: 13, title: "Haseen", artist: "Talwinder" },
+  { id: 14, title: "Pasoori", artist: "Ali Sethi & Shae Gill" },
+
+  // Hindi / Bollywood Hits
+  { id: 15, title: "Kesariya", artist: "Arijit Singh" },
+  { id: 16, title: "Chaleya", artist: "Arijit Singh" },
+  { id: 17, title: "Heeriye", artist: "Jasleen Royal" },
+  { id: 18, title: "Apna Bana Le", artist: "Arijit Singh" },
+  { id: 19, title: "Tum Hi Ho", artist: "Arijit Singh" },
+  { id: 20, title: "Tere Vaaste", artist: "Varun Jain" },
+  { id: 21, title: "Maan Meri Jaan", artist: "King" },
+  { id: 22, title: "Tu Aake Dekhle", artist: "King" },
+  { id: 23, title: "Raataan Lambiyan", artist: "Jubin Nautiyal" },
+  { id: 24, title: "Shayad", artist: "Arijit Singh" },
+
+  // Indie / Trending India
+  { id: 25, title: "Afsos", artist: "Anuv Jain & AP Dhillon" },
+  { id: 26, title: "Husn", artist: "Anuv Jain" },
+  { id: 27, title: "Baarishein", artist: "Anuv Jain" },
+  { id: 28, title: "Gul", artist: "Anuv Jain" },
+  { id: 29, title: "Ilahi", artist: "Arijit Singh" },
+  { id: 30, title: "Safarnama", artist: "Lucky Ali" }
+];
+
+export default songs;
